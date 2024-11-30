b37f2f82d10d6fd69089d4d60a7224e3c96c7f30
not-human213 <111362026+not-human213@users.noreply.github.com>
2024-11-30 23:47:51 +0530

    Update bmp.h

--- bmp.h
+++ bmp.h
@@ -1,16 +1,5 @@
-// BMP-related data types based on Microsoft's own
-
 #include <stdint.h>
 
-/**
- * Common Data Types
- *
- * The data types in this section are essentially aliases for C/C++
- * primitive data types.
- *
- * Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
- * See http://en.wikipedia.org/wiki/Stdint.h for more on stdint.h.
- */
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
@@ -20,9 +9,6 @@ typedef uint16_t WORD;
  * BITMAPFILEHEADER
  *
  * The BITMAPFILEHEADER structure contains information about the type, size,
- * and layout of a file that contains a DIB [device-independent bitmap].
- *
- * Adapted from http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
  */
 typedef struct
 {
@@ -39,8 +25,6 @@ BITMAPFILEHEADER;
  *
  * The BITMAPINFOHEADER structure contains information about the
  * dimensions and color format of a DIB [device-independent bitmap].
- *
- * Adapted from http://msdn.microsoft.com/en-us/library/dd183376(VS.85).aspx.
  */
 typedef struct
 {
@@ -63,8 +47,6 @@ BITMAPINFOHEADER;
  *
  * This structure describes a color consisting of relative intensities of
  * red, green, and blue.
- *
- * Adapted from http://msdn.microsoft.com/en-us/library/aa922590.aspx.
  */
 typedef struct
 {
